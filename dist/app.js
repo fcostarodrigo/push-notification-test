357e76b91c70a5a3646e3b0676f53b4088d0df51
Rodrigo <fcostarodrigo@yahoo.com>
2021-12-13 09:27:17 +0000

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,31 @@
 async function subscribe() {
-  // https://github.com/web-push-libs/web-push/issues/558
-  var applicationServerKey, serviceWorkerRegister, subscription;
+  try {
+    // https://github.com/web-push-libs/web-push/issues/558
+    var applicationServerKey, serviceWorkerRegister, subscription;
 
-  applicationServerKey = await fetch("applicationServerKey.txt").then(function (
-    response
-  ) {
-    return response.text();
-  });
+    applicationServerKey = await fetch("applicationServerKey.txt").then(function (
+      response
+    ) {
+      return response.text();
+    });
 
-  console.log(await Notification.requestPermission());
+    console.log(await Notification.requestPermission());
 
-  serviceWorkerRegister = await navigator.serviceWorker.register("dist/sw.js");
+    serviceWorkerRegister = await navigator.serviceWorker.register("dist/sw.js");
 
-  subscription = await serviceWorkerRegister.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey: applicationServerKey,
-  });
+    subscription = await serviceWorkerRegister.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey,
+    });
 
-  document.querySelector("#subscription").innerHTML = JSON.stringify(
-    subscription,
-    null,
-    2
-  );
+    document.querySelector("#subscription").innerHTML = JSON.stringify(
+      subscription,
+      null,
+      2
+    );
+  } catch (error) {
+    document.querySelector("#subscription").innerHTML = error.message;
+  }
 }
 
 document.querySelector("button").addEventListener("click", subscribe);
